87bb52753eb306d23ac0ee753fb42d4021949e06
oms <journy002@naver.com>
2025-01-02 16:13:58 +0900

    "에러 수정"

--- src/main/java/com/example/calculator/calculator1.java
+++ src/main/java/com/example/calculator/calculator1.java
@@ -4,5 +4,6 @@ public class calculator1 {
     public static void main(String[] args) {
 
         System.out.println("git connection test");
+        System.out.println("git connection test2");
     }
 }
